59a3e8773f0ae492cb232a44a8b3a29190b7df63
TeonaZav <teona.zavrashvili@gmail.com>
2023-07-22 11:52:48 +0400

    feat: add tabs to the modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
-// import { ModalService } from '../services/modal.service';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule],
-  exports: [ModalComponent],
-  // providers: [ModalService],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent],
 })
 export class SharedModule {}
