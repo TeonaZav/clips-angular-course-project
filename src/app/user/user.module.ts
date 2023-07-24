f6ab9ab4f4b2b6a5b0983cb5bbf11ea281b6edcd
TeonaZav <teona.zavrashvili@gmail.com>
2023-07-24 06:10:43 +0400

    feat: add validation to login form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
-  imports: [CommonModule, SharedModule, ReactiveFormsModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule, FormsModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
