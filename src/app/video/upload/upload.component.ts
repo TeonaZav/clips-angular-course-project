b44d9d935dbeaa0661f8ec013a67549f05b8d9e5
TeonaZav <teona.zavrashvili@gmail.com>
2023-07-25 19:10:17 +0400

    feat: upload video and add to database

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
-export class UploadComponent {
+export class UploadComponent implements OnInit {
+  isDragover = false;
 
+  file: File | null = null;
+
+  nextStep = false;
+
+  showAlert = false;
+  alertMsg = 'Your clip is being uploaded...';
+  alertColor = 'blue';
+  inSubmission = false;
+  percentage = 0;
+  showPercentage = false;
+  user: firebase.User | null = null;
+
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+
+  uploadForm = new FormGroup({ title: this.title });
+
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipsService: ClipService
+  ) {
+    auth.user.subscribe((user) => (this.user = user));
+  }
+
+  ngOnInit(): void {}
+
+  storeFile($event: Event) {
+    this.isDragover = false;
+
+    this.file = ($event as DragEvent).dataTransfer
+      ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+      : ($event.target as HTMLInputElement).files?.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    console.log(this.file);
+
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+    this.nextStep = true;
+  }
+
+  uploadFile() {
+    console.log('file uploaded');
+    this.uploadForm.disable();
+    this.showAlert = true;
+    this.alertMsg = 'Your clip is being uploaded...';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+    this.showPercentage = true;
+
+    const clipFileName = uuid();
+    const clipPath = `clips/${clipFileName}.mp4`;
+
+    const task = this.storage.upload(clipPath, this.file);
+    const clifRef = this.storage.ref(clipPath);
+
+    task
+      .percentageChanges()
+      .subscribe((progress) => (this.percentage = (progress as number) / 100));
+
+    task
+      .snapshotChanges()
+      .pipe(
+        last(),
+        switchMap(() => clifRef.getDownloadURL())
+      )
+      .subscribe({
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title.value,
+            fileName: `${clipFileName}.mp4`,
+            url,
+          };
+
+          this.clipsService.createClip(clip);
+
+          console.log(clip);
+
+          this.alertMsg = 'Success!';
+          this.alertColor = 'green';
+          this.showPercentage = false;
+        },
+        error: (error) => {
+          this.uploadForm.enable();
+
+          this.alertColor = 'red';
+          this.alertMsg = 'Upload failed!';
+          this.inSubmission = true;
+          this.showPercentage = false;
+          console.error(error);
+        },
+      });
+  }
 }
