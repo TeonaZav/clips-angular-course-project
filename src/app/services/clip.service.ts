cfaea6ff6a2bb270c6bc86428adf0f83066c0461
TeonaZav <teona.zavrashvili@gmail.com>
2023-07-25 20:13:00 +0400

    feat: add edit component and modal

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -5,6 +5,10 @@ import {
   DocumentReference,
 } from '@angular/fire/compat/firestore';
 import IClip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { QuerySnapshot } from '@angular/fire/compat/firestore';
+import { switchMap, map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,11 +16,24 @@ import IClip from '../models/clip.model';
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>;
 
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore, private auth: AngularFireAuth) {
     this.clipsCollection = db.collection('clips');
   }
 
   createClip(data: IClip): Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data);
+  }
+
+  getUserClips() {
+    return this.auth.user.pipe(
+      switchMap((user) => {
+        if (!user) {
+          return of([]);
+        }
+        const query = this.clipsCollection.ref.where('uid', '==', user.uid);
+        return query.get();
+      }),
+      map((snapshot) => (snapshot as QuerySnapshot<IClip>).docs)
+    );
   }
 }
