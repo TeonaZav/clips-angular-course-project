28baf0405dc3ee2af3739111c62b15c77d921be0
TeonaZav <teona.zavrashvili@gmail.com>
2023-07-25 19:17:37 +0400

    feat: cancel file upload

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
 import { last, switchMap } from 'rxjs/operators';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -12,7 +15,7 @@ import { ClipService } from 'src/app/services/clip.service';
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css'],
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnDestroy {
   isDragover = false;
 
   file: File | null = null;
@@ -26,6 +29,7 @@ export class UploadComponent implements OnInit {
   percentage = 0;
   showPercentage = false;
   user: firebase.User | null = null;
+  task?: AngularFireUploadTask;
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
@@ -42,7 +46,9 @@ export class UploadComponent implements OnInit {
     auth.user.subscribe((user) => (this.user = user));
   }
 
-  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.task?.cancel();
+  }
 
   storeFile($event: Event) {
     this.isDragover = false;
@@ -72,14 +78,14 @@ export class UploadComponent implements OnInit {
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
-    const task = this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
     const clifRef = this.storage.ref(clipPath);
 
-    task
+    this.task
       .percentageChanges()
       .subscribe((progress) => (this.percentage = (progress as number) / 100));
 
-    task
+    this.task
       .snapshotChanges()
       .pipe(
         last(),
