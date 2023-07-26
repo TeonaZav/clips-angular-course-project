08590762aa0eba6f05a665fad19bdc9f16350cbb
Teona Zavrashvili <101217572+TeonaZav@users.noreply.github.com>
2023-07-27 03:04:42 +0400

    feat: render uploaded videos

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -8,23 +8,31 @@ import {
 import { ActivatedRoute, Params } from '@angular/router';
 import videojs from 'video.js';
 import Player from 'video.js/dist/types/player';
+import IClip from '../models/clip.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
   styleUrls: ['./clip.component.css'],
   encapsulation: ViewEncapsulation.None,
+  providers: [DatePipe],
 })
 export class ClipComponent implements OnInit {
-  id = '';
   @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
   player?: Player;
+  clip?: IClip;
 
   constructor(public route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.player = videojs(this.target?.nativeElement);
+    this.route.data.subscribe((data) => {
+      this.clip = data['clip'] as IClip;
 
-    this.route.params.subscribe((params: Params) => (this.id = params['id']));
+      this.player?.src({
+        src: this.clip.url,
+      });
+    });
   }
 }
